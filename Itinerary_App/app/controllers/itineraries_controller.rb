a90c67818bc2480307fa2c0c93c68ee968fc3990
Colin Craft <colin@colincraft.com>
2015-02-28 14:30:57 -0800

    Some basic Itinerary shows

--- Itinerary_App/app/controllers/itineraries_controller.rb
+++ Itinerary_App/app/controllers/itineraries_controller.rb
@@ -1,17 +1,30 @@
 class ItinerariesController < ApplicationController
+before_action :find_itinerary, only: [:show]
+
   def index
-    @itinerary = Itinerary.all
+    @itineraries = Itinerary.all
   end
 
   def edit
   end
 
   def show
+    @destinations = @itinerary.destinations
+
   end
 
   def add
   end
 
   def delete
+  end
+
+
+  private
+  def itinerary_params
+    params.require(:itinerary).permit(:name_i)
+  end
+  def find_itinerary
+    @itinerary = Itinerary.find(params[:id])
   end
 end
