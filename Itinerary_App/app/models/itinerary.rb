93dad3bfdeb4692c1cf70d37469c89becd466839
Camilo Ordonez <icoj@gmail.com>
2015-02-28 11:23:41 -0800

    camillos test commit

--- Itinerary_App/app/models/itinerary.rb
+++ Itinerary_App/app/models/itinerary.rb
@@ -3,4 +3,8 @@ class Itinerary < ActiveRecord::Base
   has_many :users, through: :destinations_itinerarys_users
   has_many :destinations
   has_many :reviews, as: :reviewable
+
+  validates :name_i, :presence => true
+  validates :origin, :presence => true
+  validates :city, :presence => true
 end
--- Itinerary_App/app/models/user.rb
+++ Itinerary_App/app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   has_many :itineraries, through: :destinations_itinerarys_users
   
   has_many :reviews, as: :reviewable
+
+  validates :first_name, :presence => true,
+  validates :last_name, :presence => true
+  validates :email, :presence => true, :uniqueness => true
+
 end
