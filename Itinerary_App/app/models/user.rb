0561abcff0277e4ebf602cbbb683c4b89e0bfea8
Colin Craft <colin@colincraft.com>
2015-02-28 11:39:39 -0800

    Added resources

--- Itinerary_App/app/models/user.rb
+++ Itinerary_App/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :itineraries, through: :destinations_itinerarys_users
   has_many :reviews, as: :reviewable
 
-  validates :first_name, :presence => true,
+  validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :email, :presence => true, :uniqueness => true
 
