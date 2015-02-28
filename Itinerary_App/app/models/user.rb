e47401fe40af3ac9a04f1b8ad5208bed1adf5528
Camilo Ordonez <icoj@gmail.com>
2015-02-28 12:52:40 -0800

    Test

--- Itinerary_App/app/models/user.rb
+++ Itinerary_App/app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   validates :first_name, :presence => true, length: {minimum: 4, maximum: 25}
   validates :last_name, :presence => true, length: {minimum: 4, maximum: 25}
   validates :email, :presence => true, :uniqueness => true
-
+  
 end
