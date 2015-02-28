e0c3a9b3524fb6b32eb9413d4e92586c3f3c74ee
Camilo Ordonez <icoj@gmail.com>
2015-02-28 11:28:48 -0800

    Camilo's slack integration commit

--- Itinerary_App/app/models/user.rb
+++ Itinerary_App/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   
   has_many :reviews, as: :reviewable
 
-  validates :first_name, :presence => true,
+  validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :email, :presence => true, :uniqueness => true
 
